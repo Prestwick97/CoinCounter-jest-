7df3c426ca57eead57e07350def269587c822325
Prestwick97 <prestwick97@gmail.com>
2020-04-13 10:29:39 -0700

    Green. Pass second test for limiting decimal length to two.
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- src/counter.js
+++ src/counter.js
@@ -3,6 +3,20 @@ export function Counter(number) {
     return "invalid input";
   }
   else {
-    return number;
+    var decimalPlaces = countDecimals(number);
+    if (decimalPlaces > 2) {
+      return "invalid input";
+    }
+    else {
+      return number;
+    }
+  }
+}
+
+var countDecimals = function(number) {
+  if(Math.floor(number.valueOf()) == number.valueOf()) {
+    return 0;
+  } else {
+    return number.toString().split(".")[1].length || 0;
   }
 }
