559f0495815c482145561e75b13e438d99df4ddd
Prestwick97 <prestwick97@gmail.com>
2020-04-13 10:43:42 -0700

    Red. Fail third test to return amount of quarters in a given dollar amount.
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -8,7 +8,12 @@ describe('Counter', () => {
   });
 
   test('should return error message if arguments are not a number with 0-2 decimal places', () => {
-    var counter = Counter(5.987)
+    var counter = Counter(5.987);
     expect(counter).toBe("invalid input");
+  });
+
+  test('should return number of quarters that make up the number', () => {
+    var counter = Counter(4.99);
+    expect(counter).toBe(19);
   });
 });
