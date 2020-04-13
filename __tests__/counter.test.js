adc2811eae55cb8e5356c27c68c6ad5bfc16e858
Prestwick97 <prestwick97@gmail.com>
2020-04-13 10:02:14 -0700

    Green. Pass first test
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Geoff Goetz <geoff.goetz503@gmail.com>

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -3,11 +3,11 @@ import { Counter } from './../src/counter.js';
 describe('Counter', () => {
 
   test('should return error message if argument is not a number', () => {
-    var counter = new Counter("hello");
-    expect(counter.toEqual("invalid input"));
+    const counter = Counter("hello");
+    expect(counter).toBe("invalid input");
   });
 
   // test('should return error message if arguments are not a number with 0-2 decimal places', () => {
-  //   var counter = new Counter(5.987)
+  //   var counter = Counter(5.987)
   // });
 });
--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,8 @@
 export function Counter(number) {
-  // if (isNaN(number)) {
-  //   return "invalid input";
-  // }
-  // else {
+  if (isNaN(number)) {
+    return "invalid input";
+  }
+  else {
     return number;
-  // }
+  }
 }
